8df1fad6bf6ac8a4b2887a7df9d98d34efabe300
Vukile2801w <wukbg10@gmail.com>
2025-04-15 22:00:41 +0200

    Finalne promene dopisane uz Rad

--- Chat_Room/Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++ Chat_Room/Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3315f0f924494dfa9359ecde5bc3078039711b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9127a6638987d4696f2347d1b95ccc0017e54df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
